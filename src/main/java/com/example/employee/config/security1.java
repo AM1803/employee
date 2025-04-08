856e26ef846f1893f405f9bc9ce980d63c13a24e
AM1803 <ankitkartikmahajan@gmail.com>
2025-04-08 09:42:09 +0100

    new1

---
+++ src/main/java/com/example/employee/config/security1.java
@@ -0,0 +1,9 @@
+package com.example.employee.config;
+
+
+
+public class security1 {
+    public static void main(String[] args) {
+        System.out.println("Bello");
+    }
+}
--- src/main/java/com/example/employee/controller/tempor.java
+++
@@ -1,7 +0,0 @@
-package com.example.employee.controller;
-
-public class tempor {
-    public static void main(String[] args) {
-        System.out.println("Hello Temp");
-    }
-}
